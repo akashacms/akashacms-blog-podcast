2105fdb678c8e97b0c2a046663bef05aec9945ab
David Herron <david@davidherron.com>
2016-05-29 22:22:36 -0700

    Turn blog-news-river inside-out to use it anywhere

--- index.js
+++ index.js
@@ -36,7 +36,6 @@ module.exports = class BlogPodcastPlugin extends akasha.Plugin {
         this._config = config;
 		config.addPartialsDir(path.join(__dirname, 'partials'));
 		config.addMahabhuta(mahabhuta);
-		log('config');
 	}
 	
 	addBlogPodcast(config, name, blogPodcast) {
@@ -101,14 +100,10 @@ function findBlogDocs(config, metadata, blogcfg) {
     .then(documents => {
         documents.sort(function(a, b) {
             var aPublicationDate = Date.parse(
-                    a.metadata.publicationDate
-                  ? a.metadata.publicationDate
-                  : a.stat.mtime
+                a.metadata.publicationDate ? a.metadata.publicationDate : a.stat.mtime
             );
             var bPublicationDate = Date.parse(
-                    b.metadata.publicationDate
-                  ? b.metadata.publicationDate
-                  : b.stat.mtime
+                b.metadata.publicationDate ? b.metadata.publicationDate : b.stat.mtime
             );
             if (aPublicationDate < bPublicationDate) return -1;
             else if (aPublicationDate === bPublicationDate) return 0;
@@ -125,50 +120,53 @@ function findBlogDocs(config, metadata, blogcfg) {
 
 var mahabhuta = [
 	function($, metadata, dirty, done) {
-        if (! metadata.blogtag) {return done(); }
-        if (!metadata.config.blogPodcast) { return done(); }
-		var blogcfg = metadata.config.blogPodcast[metadata.blogtag];
-        if (!blogcfg) return done(new Error('No blog configuration found for blogtag '+ metadata.blogtag));
-        if (! metadata.config.blogPodcast.hasOwnProperty(metadata.blogtag)) {
-            return done(new Error("no blogPodcast item for "+ metadata.blogtag));
-        }
-		// log('blog-news-river '+ metadata.document.path +' '+ util.inspect(blogcfg));
+		
 		var elements = [];
 		$('blog-news-river').each(function(i, elem) { elements.push(elem); });
 		if (elements.length > 0) {
-			// log('blog-news-river');
-			findBlogDocs(metadata.config, metadata, blogcfg)
-            .then(documents => {
+			async.eachSeries(elements, (element, next) => {
+				var blogtag = $(element).attr("blogtag");
+				if (!blogtag) {
+					blogtag = metadata.blogtag;
+				}
+				if (!blogtag) {// no blog tag, skip? error?
+					error("NO BLOG TAG "+ metadata.document.path);
+					return done(new Error("NO BLOG TAG "+ metadata.document.path));
+				}
 				
-				// log('blog-news-river documents '+ util.inspect(documents));
-			
-				var count = 0;
-				var documents2 = documents.filter(doc => {
-					if (typeof maxEntries === "undefined"
-					|| (typeof maxEntries !== "undefined" && count++ < maxEntries)) {
-						return true;
-					} else return false;
-				});
-				// log('blog-news-river documents2 '+ util.inspect(documents2));
-			
-				var rssitems   = documents2.map(doc => {
-					return {
-						title: doc.metadata.title,
-						description: doc.metadata.teaser ? doc.metadata.teaser : "",
-						url: metadata.config.root_url +'/'+ doc.renderpath,
-						date: doc.metadata.publicationDate
-							? doc.metadata.publicationDate
-							: doc.stat.mtime
-					};
-				});
-				// log('blog-news-river rssitems '+ util.inspect(rssitems));
+				// log('blog-news-river '+ blogtag +' '+ metadata.document.path);
 				
-                async.eachSeries(elements, (element, next) => {
-                    var maxEntries = $(element).attr('maxentries');
-                    
-                    // console.log(element.name +' '+ metadata.blogtag);
-                    
-                    
+				var blogcfg = metadata.config.blogPodcast[blogtag];
+				if (!blogcfg) return done(new Error('No blog configuration found for blogtag '+ blogtag));
+			
+				var maxEntries = $(element).attr('maxentries');
+				
+				var template = $(element).attr("template");
+				if (!template) template = "blog-news-river.html.ejs";
+				
+				findBlogDocs(metadata.config, metadata, blogcfg)
+				.then(documents => {
+					
+					// log('blog-news-river documents '+ util.inspect(documents));
+				
+					var count = 0;
+					var documents2 = documents.filter(doc => {
+						if (typeof maxEntries === "undefined"
+						|| (typeof maxEntries !== "undefined" && count++ < maxEntries)) {
+							return true;
+						} else return false;
+					});
+					// log('blog-news-river documents2 '+ util.inspect(documents2));
+				
+					var rssitems   = documents2.map(doc => {
+						return {
+							title: doc.metadata.title,
+							description: doc.metadata.teaser ? doc.metadata.teaser : "",
+							url: metadata.config.root_url +'/'+ doc.renderpath,
+							date: doc.metadata.publicationDate ? doc.metadata.publicationDate : doc.stat.mtime
+						};
+					});
+					
                     akasha.generateRSS(metadata.config, blogcfg, {
                             feed_url: metadata.config.renderDestination + blogcfg.rssurl,
                             pubDate: new Date()
@@ -176,7 +174,9 @@ var mahabhuta = [
                         rssitems, blogcfg.rssurl)
 					.catch(err => { error(err); });
                     
-                    akasha.partial(metadata.config, "blog-news-river.html.ejs", {
+					// console.log(util.inspect(documents2));
+					
+                    akasha.partial(metadata.config, template, {
                         documents: documents2,
                         feedUrl: blogcfg.rssurl
                     })
@@ -185,12 +185,14 @@ var mahabhuta = [
 						next();
                     })
 					.catch(err => { next(err); });
-                },
-                function(err) {
-                    if (err) done(err);
-                    else done();
-                });
-            });
+				})
+				.catch(err => { done(err); });
+			},
+			function(err) {
+				if (err) done(err);
+				else done();
+			});
+
 		} else done();
     },
 	
@@ -240,50 +242,5 @@ var mahabhuta = [
             })
             .catch(err => { done(err); });
 		} else done();
-		/* akasha.readDocumentEntry(metadata.documentPath, function(err, docEntry) {
-			$('blog-next-prev').each(function(i, elem) { elements.push(elem); });
-			if (elements.length > 0) {
-				if (!blogcfg) {
-					return done(new Error('No blog configuration found for blogtag '+ metadata.blogtag));
-				} else {
-					documents = findBlogDocs(config, metadata, blogcfg);
-				}
-			}
-			async.eachSeries(elements, function(element, next) {
-				// what's the current document
-				// find it within documents
-				var docIndex = -1;
-				for (var j = 0; j < documents.length; j++) {
-					if (documents[j].path === docEntry.path) {
-						docIndex = j;
-					}
-				}
-				if (docIndex >= 0) {
-					var prevDoc = docIndex === 0 ? documents[documents.length - 1] : documents[docIndex - 1];
-					var nextDoc = docIndex === documents.length - 1 ? documents[0] : documents[docIndex + 1];
-					akasha.partial('blog-next-prev.html.ejs', {
-						prevDoc: prevDoc, nextDoc: nextDoc, thisDoc: docEntry, documents: documents
-					}, function(err, html) {
-						if (err) next(err);
-						else {
-							$(element).replaceWith(html);
-							next();
-						}
-					});
-				} else {
-					next(new Error('did not find document in blog'));
-				}
-				// prevDoc =
-				// nextDoc =
-				// akasha.partial('blog-next-prev.html.ejs', {
-				//		prevDoc: prevDoc, nextDoc: nextDoc
-				// })
-				// next();
-			},
-			function(err) {
-				if (err) done(err);
-				else done();
-			});
-		}); */
     }
 ];
